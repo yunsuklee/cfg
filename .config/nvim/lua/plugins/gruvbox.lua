77b34d837761ca0fd1cbf83459ce7c28a478e22a
mr-sergito <sergioleesp@gmail.com>
2025-06-29 19:15:27 +0200

    Add vanilla nvim config

---
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,11 @@
+return {
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	config = function()
+		require("gruvbox").setup({
+			contrast = "hard",
+			transparent_mode = false,
+		})
+		vim.cmd("colorscheme gruvbox")
+	end,
+}
