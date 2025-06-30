c2c140a89965bebf8cd1fdd8f5e675a404cd691e
mr-sergito <sergioleesp@gmail.com>
2025-07-01 00:11:24 +0200

    Add line number to nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,5 @@ vim.opt.tabstop = 4      -- Number of spaces a tab counts for
 vim.opt.shiftwidth = 4   -- Number of spaces for auto-indent
 vim.opt.softtabstop = 4  -- Number of spaces a tab counts for while editing
 vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.number = true
+vim.opt.relativenumber = true
