d8c9bded3a734486436a2baa73e7878cc5075155
mr-sergito <sergioleesp@gmail.com>
2025-07-02 21:07:44 +0200

    nvim kickstart

--- .config/nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
--- Tab settings
-vim.opt.tabstop = 4      -- Number of spaces a tab counts for
-vim.opt.shiftwidth = 4   -- Number of spaces for auto-indent
-vim.opt.softtabstop = 4  -- Number of spaces a tab counts for while editing
-vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.number = true
-vim.opt.relativenumber = true
