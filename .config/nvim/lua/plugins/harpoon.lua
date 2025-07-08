a3f2f65e8738298a6e230645e5657b79d50fe94e
sergioyunsuk_lee <sergioyunsuk_lee@bluesign.com>
2025-07-08 10:54:59 +0200

    Use default harpoon ui

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -12,27 +12,9 @@ return {
     {
       '<leader>e',
       function()
-        local harpoon = require 'harpoon'
-        local conf = require('telescope.config').values
-        local function toggle_telescope(harpoon_files)
-          local file_paths = {}
-          for _, item in ipairs(harpoon_files.items) do
-            table.insert(file_paths, item.value)
-          end
-          require('telescope.pickers')
-            .new({}, {
-              prompt_title = 'Harpoon',
-              finder = require('telescope.finders').new_table {
-                results = file_paths,
-              },
-              previewer = conf.file_previewer {},
-              sorter = conf.generic_sorter {},
-            })
-            :find()
-        end
-        toggle_telescope(harpoon:list())
+        require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())
       end,
-      desc = 'Open harpoon telescope menu',
+      desc = 'Open harpoon menu',
     },
     {
       '<leader>1',
