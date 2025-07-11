f33c68a8c670eabae4d50ebd9ffa5ca37905ac02
Sergio Yunsuk Lee <sergioleesp@gmail.com>
2025-07-11 20:37:09 +0200

    Remove deprecated rustfmt

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -271,7 +271,6 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'rustfmt', -- Used to format Rust code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
