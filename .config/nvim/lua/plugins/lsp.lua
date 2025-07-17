cacbdc0bcb53ff9fdd901fd69cce4ca56bb7c13d
sergioyunsuk_lee <sergioyunsuk_lee@bluesign.com>
2025-07-17 03:51:04 +0200

    unnecessary ensure installed formatters

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -281,10 +281,10 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'clang-format',
-        'prettier',
-        'prettierd',
-        'eslint_d',
+        -- 'clang-format',
+        -- 'prettier',
+        -- 'prettierd',
+        -- 'eslint_d',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
