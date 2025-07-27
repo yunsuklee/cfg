c41ef4ca7d9d0e2605452ca501118384cb4f751d
Sergio Yunsuk Lee <sergioleesp@gmail.com>
2025-07-27 20:28:31 +0200

    nvim status line total lines

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -189,7 +189,7 @@ return {
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return '%2l/%L%m'
       end
 
       -- ... and there is more!
