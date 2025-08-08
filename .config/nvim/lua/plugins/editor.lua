e971a86b4e15b12ac7fa17bb3ed06ad07e9bdffe
sergioyunsuk_lee <sergioyunsuk_lee@bluesign.com>
2025-08-08 16:39:28 +0200

    nvim add column

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -181,10 +181,10 @@ return {
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
+      -- cursor location to LINE:COLUMN:TOTAL_LINES
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%L'
+        return '%2l:%2c:%L'
       end
 
       -- Remove filename from statusline for all windows
