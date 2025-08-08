5ece20f22980d2ea207fbd21937fd77045c9707d
sergioyunsuk_lee <sergioyunsuk_lee@bluesign.com>
2025-08-08 16:16:09 +0200

    add lite-xl

---
+++ .config/lite-xl/user_settings.lua
@@ -0,0 +1,16 @@
+return {
+  ["config"] = {
+    ["borderless"] = true,
+    ["custom_keybindings"] = {
+      ["core:force-quit"] = {
+        [1] = "ctrl+q"
+      }
+    },
+    ["mouse_wheel_scroll"] = 100.0,
+    ["plugins"] = {
+      ["treeview"] = {
+        ["visible"] = true
+      }
+    }
+  }
+}
